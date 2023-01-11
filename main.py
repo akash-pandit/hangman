246f3789cde4e098aa99bbdd65ad913b167817ac
Akash Pandit <34327539+akash-pandit@users.noreply.github.com>
2023-01-11 13:01:08 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,114 @@
+from random import randrange as rr
+
+
+def display_word_char(base_word: list, guessed_word: list, guess_: str):
+    """Changes the visuals of the guessed word (underscores), deducts lives, and handles guessing for
+    all inputs of a single character"""
+    if guess_ not in base_word:
+        # deducts a life from the user if their guess is not a letter in the word
+        global lives
+        lives -= 1
+        print('Remaining Lives:', lives, '(-1)')
+    elif guess_ in guessed_word:
+        # if the user guesses a letter they've guessed before, this tells them that and does not deduct a life
+        print('Please provide a letter(or letters) you have not already guessed!\nYour lives remain unchanged.')
+        print('Remaining Lives:', lives)
+    else:
+        # if the user guesses a letter in the word, as long as that letter is in the word the program will loop through
+        # and add that letter to the underscores (guessed_word) where it should go and remove it from the base word so
+        # that when all occurrences of that letter are gone, the loop will end
+        base_word = list(base_word)
+        while guess_ in base_word:
+            for i in range(len(base_word)):
+                if base_word[i] == guess_:
+                    base_word[i] = '_'
+                    guessed_word[i] = guess_
+        print('Remaining Lives:', lives)
+
+    return ''.join(guessed_word)  # returns the guessed word joined together in string format
+
+
+def display_word_chars(base_word: list, guessed_word: list, guess_: str):
+    """Changes the visuals of the guessed word (underscores), deducts lives, and handles guessing for
+        all inputs of multiple characters"""
+    global lives  # func can now edit the global var lives
+    base_word = ''.join(base_word)
+    if guess_ == base_word:
+        # checks for if the user gets the whole word right first try
+        print('Remaining Lives:', lives)
+        return guess_
+    else:
+        total_chars = len(set(guess_))
+        correct_chars = 0
+        for i in list(set(guess_)):
+            if i in base_word:
+                correct_chars += 1
+        print(f'Sorry, that was not the correct word. \n'
+              f'Out of the {total_chars} unique letters in that word, {correct_chars} were correct.')
+        if correct_chars == len(set(base_word)):
+            print('Even though your guess contains every unique letter in the word, you went just a bit overboard.')
+        lives -= 1
+        print('Remaining Lives:', lives, '(-1)')
+        return ''.join(guessed_word)
+
+
+def display_choices(letters_: str, guessword: str):
+    letters_ = list(letters_)
+    for letter in guessword:
+        if letter.upper() in letters_:
+            letters_[letters_.index(letter.upper())] = '_'
+    return ''.join(letters_)
+
+
+if __name__ == '__main__':
+    print('=' * 100)
+    print('Welcome to Hangman! Or Guess The Word, given that there is no hanging man.'
+          '\nYou determine how many lives you get, which represent the amount of times you'
+          '\ncan mess up before you lose.')
+    print('For each guess you can only guess english letters, both capital and lowercase.\n'
+          '\nIf you guess more than 1 letter in the guess, the program will assume you are'
+          '\ntrying to guess the whole word and only fill in letters if you get the word correct.'
+          '\nIf you do not get the word correct, the program will deduct 1 life and tell you how'
+          '\nmany (but not which) unique letters you got correct in your guess.'
+          '\n\nGood luck and have fun!\n')
+    print('='*100)
+
+    with open('words.txt') as words:
+        wordlist = [word for word in words.read().split()]
+
+    word = wordlist[rr(len(wordlist))].lower()
+    guess_word = list('_'*len(word))
+    _word = word
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    lives = input('How many lives would you like? ')
+    while not lives.isdigit() or int(lives) < 1:
+        lives = input('Please provide a positive integer number of lives.\nHow many lives would you like? ')
+    lives = int(lives)
+
+    # print(word)  # UNCOMMENT THIS FOR TESTING PURPOSES
+
+    while '_' in guess_word:
+        if lives > 0:
+            print('='*50, '\n\n', ' '*4, ''.join(guess_word), '\n' + '='*50)
+            guess = input('Guess a letter: ').lower().replace(' ', '', -1)
+
+            while len(guess) < 1 or not guess.isalpha():
+                guess = input('Please provide a letter (or letters) for a guess.\nTake your guess: ')
+            if len(guess) > 1:
+                guess_word = list(display_word_chars(_word, guess_word, guess))
+            else:
+                guess_word = list(display_word_char(_word, guess_word, guess))
+            letters = display_choices(letters, guess_word)
+            print("Remaining Not Guessed Letters:", ' '.join(list(letters)))
+
+        else:
+            print('You lost! The word was:', word)
+            break
+
+    if lives > 0:
+        print('='*50, '\n\n', ' '*4, ''.join(guess_word), '\n' + '='*50)
+        print('Congratulations! You won!')
+
+input('[Press ENTER to quit]')
+
